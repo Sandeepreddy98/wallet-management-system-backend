9504ad0541027b847c7c6f3a04362e4823aa8c7c
Sandeep Reddy <52911183+Sandeepreddy98@users.noreply.github.com>
2025-03-10 23:01:53 +0530

    Update const.js

--- src/constants/const.js
+++ src/constants/const.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+console.log(process.env.MONGO_URI)
 const MONGO_URI= process.env.MONGO_URI
 const PORT=process.env.PORT
 const SERVER_ADDRESS = process.env.SERVER_ADDRESS
